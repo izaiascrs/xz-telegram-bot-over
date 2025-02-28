428833d4896d7fa2799a35e4652f60f3e371b887
izaiascrs <40672426+izaiascrs@users.noreply.github.com>
2025-02-27 22:33:32 -0300

    Update index.ts

--- src/backtest/index.ts
+++ src/backtest/index.ts
@@ -7,7 +7,7 @@ export async function getBackTestResults() {
   let optimizer: ConfigOptimizer | undefined;
   
   const digitStrategies = Array.from({ length: 10 }).map(
-    (_, i) => new ThreeAboveStrategy({ entryDigit: i, compareDigit: 5 })
+    (_, i) => new ThreeAboveStrategy({ entryDigit: i, compareDigit: 4 })
   );
 
   try {
@@ -24,7 +24,7 @@ export async function getBackTestResults() {
     const digitsTradesHistory = backTestResults.map(
       (backTest) => backTest.digitStats
     );
-    optimizer = new ConfigOptimizer(digitsTradesHistory, 6);
+    optimizer = new ConfigOptimizer(digitsTradesHistory, 5);
   } catch (error) {
     console.error("Erro ao executar backtest:", error);
   }
