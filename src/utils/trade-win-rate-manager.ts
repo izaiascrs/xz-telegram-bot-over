4aa0cecefcda965514f9f9f4ff912a388283493a
izaiascrs <izaiascrs@gmail.com>
2025-03-24 19:08:51 -0300

    fix percentage win rate

--- src/utils/trade-win-rate-manager.ts
+++ src/utils/trade-win-rate-manager.ts
@@ -14,7 +14,7 @@ type TradeAllStats = {
 export class TradeWinRateManger {
   private minVirtualTradeCount = 70;
   private minTradeCount = 100;
-  private minVirtualTradeWinRate = 0.43; // 42%
+  private minVirtualTradeWinRate = 0.43; // 43%
   private minTradeWinRate = 0.485; // 48.5%
   private virtualTradeStats: TradeStats = { trades: [], winRate: 0 };
   private tradeStats: TradeStats = { trades: [], winRate: 0 };
@@ -67,7 +67,7 @@ export class TradeWinRateManger {
     
 
     // between 50 and 70 trades and win rate less then 45%
-    if(tradesCount > 50 && tradesCount < 70 && this.tradeStats.winRate < 42) {
+    if(tradesCount > 50 && tradesCount < 70 && this.tradeStats.winRate < 0.42) {
       this.isAllowToTrade = false;
     }
 
